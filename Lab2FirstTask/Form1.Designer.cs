571803eda8e5af058956054177c1f503376fe0e2
Trofim Kravets <kravectrofim@gmail.com>
2025-03-16 16:45:01 +0800

    Добавьте файлы проекта.

---
+++ Lab2FirstTask/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Lab2FirstTask
+{
+  partial class Form1
+  {
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      label1 = new Label();
+      KopeckAmount = new NumericUpDown();
+      button1 = new Button();
+      checkTheme = new CheckBox();
+      ((System.ComponentModel.ISupportInitialize)KopeckAmount).BeginInit();
+      SuspendLayout();
+      // 
+      // label1
+      // 
+      label1.AutoSize = true;
+      label1.Location = new Point(12, 15);
+      label1.Name = "label1";
+      label1.Size = new Size(167, 15);
+      label1.TabIndex = 2;
+      label1.Text = "Укажите количество копеек: ";
+      // 
+      // KopeckAmount
+      // 
+      KopeckAmount.Location = new Point(185, 12);
+      KopeckAmount.Maximum = new decimal(new int[] { 9999, 0, 0, 0 });
+      KopeckAmount.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+      KopeckAmount.Name = "KopeckAmount";
+      KopeckAmount.RightToLeft = RightToLeft.No;
+      KopeckAmount.Size = new Size(88, 23);
+      KopeckAmount.TabIndex = 3;
+      KopeckAmount.Value = new decimal(new int[] { 1, 0, 0, 0 });
+      // 
+      // button1
+      // 
+      button1.BackColor = SystemColors.Window;
+      button1.ForeColor = SystemColors.ControlText;
+      button1.Location = new Point(12, 41);
+      button1.Name = "button1";
+      button1.Size = new Size(194, 23);
+      button1.TabIndex = 4;
+      button1.Text = "Вывести число рублей и копеек";
+      button1.UseVisualStyleBackColor = false;
+      button1.Click += button1_Click;
+      // 
+      // checkTheme
+      // 
+      checkTheme.AutoSize = true;
+      checkTheme.Location = new Point(12, 70);
+      checkTheme.Name = "checkTheme";
+      checkTheme.Size = new Size(95, 19);
+      checkTheme.TabIndex = 5;
+      checkTheme.Text = "Тёмная тема";
+      checkTheme.UseVisualStyleBackColor = true;
+      checkTheme.CheckedChanged += checkBox1_CheckedChanged;
+      // 
+      // Form1
+      // 
+      AutoScaleDimensions = new SizeF(7F, 15F);
+      AutoScaleMode = AutoScaleMode.Font;
+      BackColor = SystemColors.Window;
+      ClientSize = new Size(284, 111);
+      Controls.Add(checkTheme);
+      Controls.Add(button1);
+      Controls.Add(KopeckAmount);
+      Controls.Add(label1);
+      Name = "Form1";
+      StartPosition = FormStartPosition.CenterScreen;
+      Text = "Конвертер копеек";
+      ((System.ComponentModel.ISupportInitialize)KopeckAmount).EndInit();
+      ResumeLayout(false);
+      PerformLayout();
+    }
+
+    #endregion
+    private Label label1;
+    private NumericUpDown KopeckAmount;
+    private Button button1;
+    private CheckBox checkTheme;
+  }
+}
---
+++ Lab2FirstTask/Form1.cs
@@ -0,0 +1,62 @@
+﻿namespace Lab2FirstTask
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      // Получаем значение из NumericUpDown
+      decimal value = KopeckAmount.Value;
+
+      // Формируем сообщение для отображения
+      string message = $"Вы ввели {value} копеек.";
+
+      // Показываем сообщение с измененным заголовком и иконкой
+      MessageBox.Show(message, "Вывод ответа", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+
+    private void checkBox1_CheckedChanged(object sender, EventArgs e)
+    {
+      if (checkTheme.Checked) // Если CheckBox отмечен
+      {
+        // Устанавливаем темные цвета для формы
+        checkTheme.BackColor = Color.DimGray; // Темный фон
+        checkTheme.ForeColor = Color.White; // Белый текст
+
+        // Устанавливаем цвета для элементов управления
+        button1.BackColor = Color.DimGray;
+        button1.ForeColor = Color.White;
+
+        KopeckAmount.BackColor = Color.DimGray;
+        KopeckAmount.ForeColor = Color.White;
+
+        this.BackColor = Color.DimGray;
+        this.ForeColor = Color.White;
+      }
+      else // Если CheckBox не отмечен
+      {
+        // Возвращаем все в светлое состояние
+        checkTheme.BackColor = Color.White; // Светлый фон
+        checkTheme.ForeColor = Color.Black; // Черный текст
+
+        // Устанавливаем цвета для элементов управления
+        button1.BackColor = Color.White; // Светлый фон для кнопки
+        button1.ForeColor = Color.Black; // Черный текст для кнопки
+
+        KopeckAmount.BackColor = Color.White; // Светлый фон для другого элемента
+        KopeckAmount.ForeColor = Color.Black; // Черный текст для другого элемента
+
+        this.BackColor = Color.White; // Светлый фон для формы
+        this.ForeColor = Color.Black; // Черный текст для формы
+      }
+    }
+  }
+}
